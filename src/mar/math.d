ba9f9c2adf94a918a279caf21c152db8c23e04ab
Jonathan Marler <johnnymarler@gmail.com>
2019-05-07 22:17:03 -0600

    fix sin function

--- src/mar/math.d
+++ src/mar/math.d
@@ -1,6 +1,12 @@
 module src.mar.math;
 
-/// intrinsic
-extern (C) real sin(real x) @safe pure nothrow @nogc;
-double sin(double x) @safe pure nothrow @nogc { return sin(cast(real) x); }
-float sin(float x) @safe pure nothrow @nogc { return sin(cast(real) x); }
+version (NoStdc)
+{
+    // sin function not implemented yet
+}
+else
+{
+    extern (C) double sin(double x) @safe pure nothrow @nogc;
+    private extern (C) float sinf(float x) @safe pure nothrow @nogc;
+    float sin(float x) @safe pure nothrow @nogc { pragma(inline, true); return sinf(x); }
+}
